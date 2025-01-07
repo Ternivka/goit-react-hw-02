3a74292c851810f21364658fde325a70707ce6e5
yeah <154704827+Ternivka@users.noreply.github.com>
2025-01-08 00:29:59 +0100

    fixed wrong path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 // import { useState } from "react";
 import "./App.css";
-import Description from "./Description/description";
+import Description from "./Description/Description";
 
 function App() {
   // const [feedback, setFeedback] = useState({
